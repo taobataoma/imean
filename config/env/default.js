bfd27861e411f076a4ec881520a70d7149a10205
OldHawk <taobataoma@gmail.com>
2017-05-01 00:09:55 +0800

    modify app keywords and desc

--- config/env/default.js
+++ config/env/default.js
@@ -3,8 +3,8 @@
 module.exports = {
   app: {
     title: 'MEAN.JS',
-    description: 'Full-Stack JavaScript with MongoDB, Express, AngularJS, and Node.js',
-    keywords: 'mongodb, express, angularjs, node.js, mongoose, passport',
+    description: 'Full-Stack JavaScript with Sequelize, Express, AngularJS, and Node.js',
+    keywords: 'sequelize, express, angularjs, node.js, mongoose, passport',
     googleAnalyticsTrackingID: process.env.GOOGLE_ANALYTICS_TRACKING_ID || 'GOOGLE_ANALYTICS_TRACKING_ID'
   },
   // mysql db settings
